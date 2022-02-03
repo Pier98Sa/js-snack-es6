d6781ee50b5a1afcbe9f30c2eac2091342062fcf
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-02-03 17:37:41 +0100

    creato l'array

---
+++ snack2/main.js
@@ -0,0 +1,122 @@
+const SerieA = [
+    {
+        'nome': 'Atalanta',
+        'punti fatti':0,
+        'falli subiti':0,
+    },
+
+    {
+        'nome':'Bologna',
+        'punti fatti':0,
+        'falli subiti':0,
+    },
+
+    {
+        'nome':'Cagliari',
+        'punti fatti':0,
+        'falli subiti':0,
+    },
+
+    {
+        'nome':'Empoli',
+        'punti fatti':0,
+        'falli subiti':0,
+    },
+
+    {
+        'nome':'Fiorentina',
+        'punti fatti':0,
+        'falli subiti':0,
+    },
+
+    {
+        'nome':'Genoa',
+        'punti fatti':0,
+        'falli subiti':0,
+    },
+
+    {
+        'nome':'Inter',
+        'punti fatti':0,
+        'falli subiti':0,
+    },
+
+    {
+        'nome':'Juventus',
+        'punti fatti':0,
+        'falli subiti':0,
+    },
+
+    {
+        'nome':'Lazio',
+        'punti fatti':0,
+        'falli subiti':0,
+    },
+
+    {
+        'nome':'Milan',
+        'punti fatti':0,
+        'falli subiti':0,
+    },
+
+    {
+        'nome':'Napoli',
+        'punti fatti':0,
+        'falli subiti':0,
+    },
+
+    {
+        'nome':'Roma',
+        'punti fatti':0,
+        'falli subiti':0,
+    },
+
+    {
+        'nome':'Salernitana',
+        'punti fatti':0,
+        'falli subiti':0,
+    },
+
+    {
+        'nome':'Sampdoria',
+        'punti fatti':0,
+        'falli subiti':0,
+    },
+
+    {
+        'nome':'Sassuolo',
+        'punti fatti':0,
+        'falli subiti':0,
+    },
+
+    {
+        'nome':'Spezia',
+        'punti fatti':0,
+        'falli subiti':0,
+    },
+
+    {
+        'nome':'Torino',
+        'punti fatti':0,
+        'falli subiti':0,
+    },
+
+    {
+        'nome':'Udinese',
+        'punti fatti':0,
+        'falli subiti':0,
+    },
+
+    {
+        'nome':'Venezia',
+        'punti fatti':0,
+        'falli subiti':0,
+    },
+
+    {
+        'nome':'Verona',
+        'punti fatti':0,
+        'falli subiti':0,
+    }
+
+]
