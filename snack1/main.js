5686f5168761d218f93986cfa54d8ca8be66b12e
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-02-03 16:01:18 +0100

    creato l'array

---
+++ snack1/main.js
@@ -0,0 +1,27 @@
+const bici = [
+    {
+        'nome' : Savadeck,
+        'peso' : 8
+    },
+
+    {
+        'nome' : NAZGGYA ,
+        'peso' : 7
+    },
+
+    {
+        'nome' : Canyon ,
+        'peso' : 6.5
+    },
+
+    {
+        'nome' : Rose,
+        'peso' : 6.9
+    },
+
+    {
+        'nome' : Sarto,
+        'peso' : 5.7
+    }
+    
+];
